ba1717ecbddd206ebf544f5057ae52960552d1de
Diego Quipuzco <demential2003@gmail.com>
2025-04-19 01:05:03 -0500

    Implementación de sistema de respuestas de la API

---
+++ src/main/java/com/example/loudlygmz/utils/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.example.loudlygmz.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private int statusCode;
+    private boolean success;
+    private String message;
+    private T data;
+
+    // Constructor para respuestas exitosas
+    public static <T> ApiResponse<T> success(int statusCode, String message, T data) {
+        return new ApiResponse<>(statusCode, true, message, data);
+    }
+
+    // Constructor para respuestas de error
+    public static <T> ApiResponse<T> error(int statusCode, String message, T data) {
+        return new ApiResponse<>(statusCode, false, message, data);
+    }
+}
