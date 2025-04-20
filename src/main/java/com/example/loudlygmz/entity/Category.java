400600d3f1b0c938834494f96311335289768505
Diego Quipuzco <demential2003@gmail.com>
2025-04-20 13:36:25 -0500

    Se implementó @Data de lombok

--- src/main/java/com/example/loudlygmz/entity/Category.java
+++ src/main/java/com/example/loudlygmz/entity/Category.java
@@ -10,47 +10,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "categories")
+@Data
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name, description;
+    
     @ManyToMany(mappedBy = "categories") // Relación inversa, gestionada por Category
     @JsonBackReference
     private List<Game> games;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List<Game> getGames() {
-        return games;
-    }
-
-    public void setGames(List<Game> games) {
-        this.games = games;
-    }
 }
