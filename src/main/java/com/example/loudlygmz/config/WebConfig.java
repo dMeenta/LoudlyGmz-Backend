43972dc01b6f318f9f3f9e8e343cb1e725185570
Diego Quipuzco <demential2003@gmail.com>
2025-04-20 13:33:10 -0500

    Se arreglo el CORS para el frontend

--- src/main/java/com/example/loudlygmz/config/WebConfig.java
+++ src/main/java/com/example/loudlygmz/config/WebConfig.java
@@ -1,10 +1,11 @@
 package com.example.loudlygmz.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.google.api.client.util.Value;
-
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${frontend.url}")
