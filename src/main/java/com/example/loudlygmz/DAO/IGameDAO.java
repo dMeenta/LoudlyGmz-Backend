ebc5e58485fe0626ec8616c2b27ef2a5eabb86cb
Diego Quipuzco <demential2003@gmail.com>
2025-05-01 21:29:51 -0500

    Limpieza del código

--- src/main/java/com/example/loudlygmz/DAO/IGameDAO.java
+++ src/main/java/com/example/loudlygmz/DAO/IGameDAO.java
@@ -9,12 +9,6 @@ import org.springframework.data.repository.query.Param;
 import com.example.loudlygmz.entity.Game;
 
 public interface IGameDAO extends JpaRepository<Game, Integer> {
-    /*
-     * @Query(value = "SELECT g.* FROM games g " +
-     * "JOIN games_categories gc ON g.id = gc.game_id " +
-     * "WHERE gc.category_id = :categoryId", nativeQuery = true)
-     * List<Game> findGamesByCategoryId(@Param("categoryId") int categoryId);
-     */
     @Query("SELECT g FROM Game g JOIN g.categories c WHERE c.id = :categoryId")
     List<Game> findGamesByCategoryId(@Param("categoryId") int categoryId);
 }
