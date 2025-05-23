8b10584c085c28e4659471f225518bf5ff79cf00
Diego Quipuzco <demential2003@gmail.com>
2025-05-23 01:43:17 -0500

    Validaciones para GameDTO

--- src/main/java/com/example/loudlygmz/application/dto/game/GameDTO.java
+++ src/main/java/com/example/loudlygmz/application/dto/game/GameDTO.java
@@ -3,15 +3,25 @@ package com.example.loudlygmz.application.dto.game;
 import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class GameDTO {
+
+    @NotBlank(message = "Game's name cannot be empty or null")
     private String name;
+    @NotBlank(message = "Game's desc cannot be empty or null")
     private String description;
+
+    @NotNull(message = "Game's release date cannot be null")
     private LocalDate release_date;
+
+    @NotBlank(message = "Game's developers cannot be empty or null")
     private String developer;
 
     private String wallpaper;
@@ -19,5 +29,6 @@ public class GameDTO {
     private String banner;
     private String logo;
 
-    private List<String> categories;
+    @NotEmpty(message = "At least one category is required")
+    private List<@NotBlank(message = "Category name cannot be blank") String> categories;
 }
