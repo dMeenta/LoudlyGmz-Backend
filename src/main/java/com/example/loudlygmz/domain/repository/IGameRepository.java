2a0ca28e9c04c5e327ec3a2644ad029cd38ef406
Diego Quipuzco <demential2003@gmail.com>
2025-05-23 00:03:03 -0500

    Fix: Se comprueba que la categoría exista

--- src/main/java/com/example/loudlygmz/domain/repository/IGameRepository.java
+++ src/main/java/com/example/loudlygmz/domain/repository/IGameRepository.java
@@ -10,5 +10,8 @@ import com.example.loudlygmz.domain.model.Game;
 
 public interface IGameRepository extends JpaRepository<Game, Integer> {
     @Query("SELECT g FROM Game g JOIN g.categories c WHERE c.id = :categoryId")
-    List<Game> findGamesByCategoryId(@Param("categoryId") int categoryId);
+    List<Game> findGamesByCategoryId(@Param("categoryId") Integer categoryId);
+
+    @Query(value = "SELECT EXISTS (SELECT 1 FROM categories WHERE id = :categoryId)", nativeQuery = true)
+    int categoryExistsRaw(@Param("categoryId") Integer categoryId);
 }
