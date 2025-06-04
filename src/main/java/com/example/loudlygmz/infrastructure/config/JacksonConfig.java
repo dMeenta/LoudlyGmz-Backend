172bad3fd6254b8a05ba8dfc019e106f49af8232
Diego Quipuzco <demential2003@gmail.com>
2025-06-04 01:34:55 -0500

    Feat: JacksonConfig 4 Instant read

---
+++ src/main/java/com/example/loudlygmz/infrastructure/config/JacksonConfig.java
@@ -0,0 +1,17 @@
+package com.example.loudlygmz.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class JacksonConfig {
+  @Bean
+  ObjectMapper objectMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModule(new JavaTimeModule()); // Soporte para LocalDateTime, Instant, etc.
+    return mapper;
+  }
+}
