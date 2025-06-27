6dd9b713f2e1df4e9891710c37e45cf868ba1383
Diego Quipuzco <demential2003@gmail.com>
2025-06-27 16:27:55 -0500

    Se quitaron los DTOs

--- src/main/java/com/example/loudlygmz/application/controller/SocialController.java
+++ src/main/java/com/example/loudlygmz/application/controller/SocialController.java
@@ -8,10 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.loudlygmz.application.dto.friendship.AcceptFriendshipRequestDTO;
-import com.example.loudlygmz.application.dto.friendship.RejectFriendshipRequestDTO;
-import com.example.loudlygmz.application.dto.friendship.SendFriendshipRequestDTO;
 import com.example.loudlygmz.domain.service.IFriendsService;
+import com.example.loudlygmz.infrastructure.common.AuthUtils;
 import com.example.loudlygmz.infrastructure.common.ResponseDTO;
 
 import jakarta.validation.Valid;
@@ -27,35 +25,32 @@ public class SocialController {
   private final IFriendsService friendsService;
 
   @PostMapping("/friend-request/send")
-  public ResponseEntity<ResponseDTO<?>> sendFriendshipRequest(@Valid @RequestBody SendFriendshipRequestDTO request) {
-    String sender = request.getSenderUsername();
-    String receiver = request.getReceiverUsername();
-    friendsService.sendFriendshipRequest(sender, receiver);
+  public ResponseEntity<ResponseDTO<String>> sendFriendshipRequest(@Valid @RequestBody String receiverUsername) {
+    String userLogged = AuthUtils.getCurrentUser().getUsername();
+    friendsService.sendFriendshipRequest(userLogged, receiverUsername);
     return ResponseEntity.ok(
       ResponseDTO.success(HttpStatus.OK.value(),
       "Friendship request sent.",
-      String.format("'%s' sent a friend request to '%s'.", sender, receiver)));
+      String.format("'%s' sent a friend request to '%s'.", userLogged, receiverUsername)));
   }
 
   @PostMapping("/friend-request/accept")
-  public ResponseEntity<ResponseDTO<?>> acceptFriendshipRequest(@Valid @RequestBody AcceptFriendshipRequestDTO request) {
-    String accepter = request.getAccepterUsername();
-    String accepted = request.getAcceptedUsername();
-    friendsService.acceptFriendshipRequest(accepter, accepted);
+  public ResponseEntity<ResponseDTO<String>> acceptFriendshipRequest(@Valid String acceptedUsername) {
+    String userLogged = AuthUtils.getCurrentUser().getUsername();
+    friendsService.acceptFriendshipRequest(userLogged, acceptedUsername);
     return ResponseEntity.ok(
       ResponseDTO.success(HttpStatus.OK.value(),
       "Friendship request accepted.",
-      String.format("'%s' accepted the friend request of '%s'.", accepter, accepted)));
+      String.format("'%s' accepted the friend request of '%s'.", userLogged, acceptedUsername)));
   }
 
   @PostMapping("/friend-request/reject")
-  public ResponseEntity<ResponseDTO<?>> rejectFriendshipRequest(@Valid @RequestBody RejectFriendshipRequestDTO request) {
-    String rejecter = request.getRejecterUsername();
-    String rejected = request.getRejectedUsername();
-    friendsService.rejectFriendshipRequest(rejecter, rejecter);
+  public ResponseEntity<ResponseDTO<String>> rejectFriendshipRequest(@Valid @RequestBody String rejectedUsername) {
+    String userLogged = AuthUtils.getCurrentUser().getUsername();
+    friendsService.rejectFriendshipRequest(userLogged, rejectedUsername);
     return ResponseEntity.ok(
       ResponseDTO.success(HttpStatus.OK.value(),
       "Friendship request rejected.",
-      String.format("'%s' rejected the friend request of '%s'.", rejecter, rejected)));
+      String.format("'%s' rejected the friend request of '%s'.", userLogged, rejectedUsername)));
   }
 }
