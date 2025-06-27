baa41f1a3782d748694ef1204eb73ce0cb812f30
Diego Quipuzco <demential2003@gmail.com>
2025-06-27 16:26:58 -0500

    se cambio el nombre

---
+++ src/main/java/com/example/loudlygmz/domain/repository/IMsqlUserRepository.java
@@ -0,0 +1,19 @@
+package com.example.loudlygmz.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.loudlygmz.domain.model.MsqlUser;
+
+public interface IMsqlUserRepository extends JpaRepository<MsqlUser, String> {
+  Optional<MsqlUser> findByUsername(String username);
+
+  @Query("SELECT u FROM MsqlUser u WHERE u.uid NOT IN :excludedIds")
+  Page<MsqlUser> findAllExcludingIds(@Param("excludedIds") List<String> excludedIds, Pageable pageable);
+}
